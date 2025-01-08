35458bf294f7aa69309493d2104b7c5ea6ed95fd
문성욱 <msw@hiand.io>
2025-01-08 11:14:13 +0900

    remove unused

--- dist/index.js
+++ dist/index.js
@@ -25,119 +25,6 @@ const IGNORED_TASK_DEFINITION_ATTRIBUTES = [
   'registeredBy'
 ];
 
-
-// Find value in a CodeDeploy AppSpec file with a case-insensitive key
-function findAppSpecValue(obj, keyName) {
-  return obj[findAppSpecKey(obj, keyName)];
-}
-
-function findAppSpecKey(obj, keyName) {
-  if (!obj) {
-    throw new Error(`AppSpec file must include property '${keyName}'`);
-  }
-
-  const keyToMatch = keyName.toLowerCase();
-
-  for (var key in obj) {
-    if (key.toLowerCase() == keyToMatch) {
-      return key;
-    }
-  }
-
-  throw new Error(`AppSpec file must include property '${keyName}'`);
-}
-
-function isEmptyValue(value) {
-  if (value === null || value === undefined || value === '') {
-    return true;
-  }
-
-  if (Array.isArray(value)) {
-    for (var element of value) {
-      if (!isEmptyValue(element)) {
-        // the array has at least one non-empty element
-        return false;
-      }
-    }
-    // the array has no non-empty elements
-    return true;
-  }
-
-  if (typeof value === 'object') {
-    for (var childValue of Object.values(value)) {
-      if (!isEmptyValue(childValue)) {
-        // the object has at least one non-empty property
-        return false;
-      }
-    }
-    // the object has no non-empty property
-    return true;
-  }
-
-  return false;
-}
-
-function emptyValueReplacer(_, value) {
-  if (isEmptyValue(value)) {
-    return undefined;
-  }
-
-  if (Array.isArray(value)) {
-    return value.filter(e => !isEmptyValue(e));
-  }
-
-  return value;
-}
-
-function cleanNullKeys(obj) {
-  return JSON.parse(JSON.stringify(obj, emptyValueReplacer));
-}
-
-function removeIgnoredAttributes(taskDef) {
-  for (var attribute of IGNORED_TASK_DEFINITION_ATTRIBUTES) {
-    if (taskDef[attribute]) {
-      core.warning(`Ignoring property '${attribute}' in the task definition file. ` +
-        'This property is returned by the Amazon ECS DescribeTaskDefinition API and may be shown in the ECS console, ' +
-        'but it is not a valid field when registering a new task definition. ' +
-        'This field can be safely removed from your task definition file.');
-      delete taskDef[attribute];
-    }
-  }
-
-  return taskDef;
-}
-
-function maintainValidObjects(taskDef) {
-  if (validateProxyConfigurations(taskDef)) {
-    taskDef.proxyConfiguration.properties.forEach((property, index, arr) => {
-      if (!('value' in property)) {
-        arr[index].value = '';
-      }
-      if (!('name' in property)) {
-        arr[index].name = '';
-      }
-    });
-  }
-
-  if (taskDef && taskDef.containerDefinitions) {
-    taskDef.containerDefinitions.forEach((container) => {
-      if (container.environment) {
-        container.environment.forEach((property, index, arr) => {
-          if (!('value' in property)) {
-            arr[index].value = '';
-          }
-        });
-      }
-    });
-  }
-  return taskDef;
-}
-
-function validateProxyConfigurations(taskDef) {
-  return 'proxyConfiguration' in taskDef && taskDef.proxyConfiguration.type && taskDef.proxyConfiguration.type == 'APPMESH' && taskDef.proxyConfiguration.properties && taskDef.proxyConfiguration.properties.length > 0;
-}
-
-
 async function run() {
   try {
     const ecs = new ECS({ customUserAgent: 'amazon-ecs-download-task-definition-for-github-actions' });
@@ -150,13 +37,11 @@ async function run() {
     core.debug('Downloading the task definition');
     const taskDefPath = path.isAbsolute(outputFile) ? outputFile : path.join(process.env.GITHUB_WORKSPACE, outputFile);
 
-    // const fileContents = fs.readFileSync(taskDefPath, 'utf8');
-    // const taskDefContents = maintainValidObjects(removeIgnoredAttributes(cleanNullKeys(yaml.parse(fileContents))));
     let registerResponse;
     try {
       // console.log("taskDefinition", taskDefinition);
       registerResponse = await ecs.describeTaskDefinition({ taskDefinition });
-      console.log("registerResponse", registerResponse);
+      // console.log("registerResponse", registerResponse);
     } catch (error) {
       core.setFailed("Failed to download task definition in ECS: " + error.message);
       // console.error(error);
@@ -170,8 +55,8 @@ async function run() {
       delete json[key];
     }
     fs.writeFileSync(taskDefPath, JSON.stringify(json, null, 4));
-    console.log(taskDefPath);
-
+    // console.log(taskDefPath);
+    
   } catch (error) {
     core.setFailed(error.message);
     core.debug(error.stack);
